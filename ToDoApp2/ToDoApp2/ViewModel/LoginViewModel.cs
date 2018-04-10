3a01c3213c4f14d37ed52c13faf844514673f3f2
artronche <arthur.tronche@etu.udamail.fr>
2018-04-10 13:41:46 +0100

    add some logic but binding doesn t work

--- ToDoApp2/ToDoApp2/ViewModel/LoginViewModel.cs
+++ ToDoApp2/ToDoApp2/ViewModel/LoginViewModel.cs
@@ -2,11 +2,54 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ToDoApp2.ViewModel
 {
-    class LoginViewModel:BaseViewModel
+    class LoginViewModel : BaseViewModel
     {
+        private string _username;
+        public string Username {
+            get => _username;
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    LoginCommand.ChangeCanExecute();
+                }
+            }
+        }
 
+        private string _password;
+        public string Password {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    LoginCommand.ChangeCanExecute();
+                }
+            }
+        }
+
+
+        public Command LoginCommand;
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(LoginExecute, CanLogin);
+        }
+
+        private bool CanLogin()
+        {
+            return !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
+        }
+
+        private void LoginExecute()
+        {
+
+        }
     }
 }
