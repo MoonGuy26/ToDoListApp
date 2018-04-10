9611921b29608936308610b20cf47f92558db84f
artronche <arthur.tronche@etu.udamail.fr>
2018-04-10 12:06:34 +0100

    Add project files.

---
+++ ToDoApp2/ToDoApp2/Model/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//
+namespace ToDoApp2.Model
+{
+    abstract class BaseModel
+    {
+        public double ID { get; private set; }
+
+    }
+}
---
+++ ToDoApp2/ToDoApp2/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp2.Model
+{
+    class User : BaseModel
+    {
+        private string _name;
+        public string Name { get => _name; private set => _name = value; }
+
+        private string _hashedPw;
+        public string HashedPw { get => _hashedPw; private set => _hashedPw = value; }
+
+        private string _note;
+        public string MyProperty
+        {
+            get { return _note; }
+            set { _note = value; }
+        }
+        
+        
+
+    }
+}
